0d3a80c7e03bd58a508b5f685e8a729febd753f5
vasudha939 <vasudha939@gmail.com>
2023-12-23 19:14:28 +0530

    push for loop

---
+++ src/DEC17/LAB001/lab002.java
@@ -0,0 +1,13 @@
+package src.DEC17.LAB001;
+
+public class lab002 {
+    public static void main(String[] args) {
+        int i;
+        for(i=1;i>=10; i++);
+        {
+            System.out.println("It is true");
+        }
+        System.out.println("End");
+
+    }
+}
