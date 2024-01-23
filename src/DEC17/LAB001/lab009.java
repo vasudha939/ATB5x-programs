9ae5153a53e2a6ce0658cffa4d069f86fc1b6a24
vasudha939 <vasudha939@gmail.com>
2024-01-23 20:49:51 +0530

    push for loop

---
+++ src/DEC17/LAB001/lab009.java
@@ -0,0 +1,13 @@
+package src.DEC17.LAB001;
+
+public class lab009 {
+    public static void main(String[] args) {
+        final boolean b1=true;
+        // b1= false;
+        for(float f=0; b1 ; ) {
+            System.out.println("V");
+
+        }
+    }
+}
+
