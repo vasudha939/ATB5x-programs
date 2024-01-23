877dd9cd6cf53d5f63f4bcbcb7ac16ef759e8107
vasudha939 <vasudha939@gmail.com>
2024-01-23 21:32:07 +0530

    push for loop

---
+++ src/DEC17/LAB001/lab15.java
@@ -0,0 +1,17 @@
+package src.DEC17.LAB001;
+
+public class lab15 {
+    public static void main(String[] args) {
+        System.out.println("I want to print the value of i");
+
+        for (int i = 10; i >0; i--) {
+
+            System.out.println("Value of" +i);
+
+        }
+
+        System.out.println("End");
+    }
+}
+
+
