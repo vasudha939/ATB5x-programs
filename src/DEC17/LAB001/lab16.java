26249623c7ce011f5db36a691bfb03806244ada8
vasudha939 <vasudha939@gmail.com>
2024-01-23 21:51:29 +0530

    push for loop

---
+++ src/DEC17/LAB001/lab16.java
@@ -0,0 +1,10 @@
+package src.DEC17.LAB001;
+
+public class lab16 {
+    public static void main(String[] args) {
+        for (int i = 1; i <10 ; i++) {
+            System.out.println("Val of "+i);
+
+        }
+    }
+}
