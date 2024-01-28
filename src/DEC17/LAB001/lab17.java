040fc0a29cbb3c8f04ada82749b4157eaa89edb5
vasudha939 <vasudha939@gmail.com>
2024-01-28 19:24:31 +0530

    push for loop

---
+++ src/DEC17/LAB001/lab17.java
@@ -0,0 +1,14 @@
+package src.DEC17.LAB001;
+
+public class lab17 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 50; ++i) {
+            if(i % 2== 0)
+            {
+                System.out.println(i+  "is even");
+            }
+
+        }
+        System.out.println("End of the program");
+    }
+}
