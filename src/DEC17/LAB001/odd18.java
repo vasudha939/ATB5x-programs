95ae5ed5e32505dc31d7986736a6c5adb1fd258d
vasudha939 <vasudha939@gmail.com>
2024-01-28 19:38:59 +0530

    push for loop

--- src/DEC17/LAB001/odd18.java
+++ src/DEC17/LAB001/odd18.java
@@ -2,12 +2,14 @@ package src.DEC17.LAB001;
 
 public class odd18 {
     public static void main(String[] args) {
-        for (int i = 0; i <50 ; i++) {
-            if(i%2!=0){
+        for (int i = 0; i < 50; i++) {
+            if((i%2)!=0)
+            {
                 System.out.println(i+  "is odd");
-            }
 
         }
-        System.out.println("End of the program");
+
+        }
+        System.out.println("End");
     }
 }
