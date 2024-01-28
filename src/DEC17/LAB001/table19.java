5776fd2c0594d986f85d656a083b1a0886b61c91
vasudha939 <vasudha939@gmail.com>
2024-01-28 19:56:01 +0530

    push for loop

---
+++ src/DEC17/LAB001/table19.java
@@ -0,0 +1,11 @@
+package src.DEC17.LAB001;
+
+public class table19 {
+    public static void main(String[] args) {
+        int num=10;
+        for (int i = 1; i <=10 ; i++) {
+            System.out.println(num  + "*" +i + "=" +(num*i));
+
+        }
+    }
+}
